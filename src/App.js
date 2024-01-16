3f7cc5d99e9d0ef69ed217cfc46693c2c5ac239a
Tommy Le <82196633+Wingingbump@users.noreply.github.com>
2024-01-16 13:25:22 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/resume" element={<Resume />} />
         </Routes>
         <Footer />
